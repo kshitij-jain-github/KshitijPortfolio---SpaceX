79e970dfbbe44c34162bacb711ae9921cdb521e1
Kshitij Jain <jainkshitij1530@gmail.com>
2025-01-10 18:06:03 +0530

    updating-responsiveness

--- site.js
+++ site.js
@@ -142,4 +142,157 @@ var swiper = new Swiper(".swiper", {
   
 
 
+//  Skills
+
+var skills = [
+  {
+    "header": "Programming Languages",
+    "captions": [
+      "C++",
+      "Java",
+      "C#",
+      "Python"
+    ],
+    "values": [
+      0.85,
+      0.90,
+      0.80,
+      0.75
+    ]
+  },
+  {
+    "header": "Frontend",
+    "captions": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap"
+    ],
+    "values": [
+      0.95,
+      0.90,
+      0.85,
+      0.80
+    ]
+  },
+  {
+    "header": "Frameworks",
+    "captions": [
+      "ASP.NET Core",
+      "MVC",
+      "Entity Framework",
+      "Ado.Net"
+    ],
+    "values": [
+      0.85,
+      0.90,
+      0.80,
+      0.75
+    ]
+  },
+  {
+    "header": "Tools & IDEs",
+    "captions": [
+      "Visual Studio",
+      "VS Code",
+      "SSMS",
+      "GitHub"
+    ],
+    "values": [
+      0.90,
+      0.85,
+      0.80,
+      0.85
+    ]
+  },
+  {
+    "header": "Database",
+    "captions": [
+      "MySQL"
+    ],
+    "values": [
+      0.85
+    ]
+  }
+];
+
+var pentagonIndex = 0;
+var valueIndex = 0;
+var width = 0;
+var height = 0;
+var radOffset = Math.PI / 2;
+var sides = 5; // Maximum number of skills per category
+var theta = 2 * Math.PI / sides; // radians per section
+
+function getXY(i, radius) {
+  return {
+    "x": Math.cos(radOffset + theta * i) * radius * width + width / 2,
+    "y": Math.sin(radOffset + theta * i) * radius * height + height / 2
+  };
+}
+
+var hue = [];
+var hueOffset = 25;
+
+for (var s in skills) {
+  $(".skills").append('<div class="pentagon" id="category-' + s + '"><div class="header"></div><div class="pentCanvas"/></div>');
+  hue[s] = (hueOffset + s * 255 / skills.length) % 255;
+}
+
+$(".pentagon").each(function (index) {
+  width = $(this).width();
+  height = $(this).height();
+  var ctx = $(this).find('canvas')[0].getContext('2d');
   
+  ctx.font = "15px Monospace";
+  ctx.textAlign = "center";
+
+  /*** LABEL ***/
+  color = "hsl(" + hue[pentagonIndex] + ", 100%, 50%)";
+  ctx.fillStyle = color;
+  ctx.fillText(skills[pentagonIndex].header, width / 2, 15);
+
+  ctx.font = "13px Monospace";
+
+  /*** PENTAGON BACKGROUND ***/
+  for (var i = 0; i < skills[pentagonIndex].captions.length; i++) {
+    ctx.beginPath();
+    xy = getXY(i, 0.3);
+    colorJitter = 25 + theta * i * 2;
+    color = "hsl(" + hue[pentagonIndex] + ",100%," + colorJitter + "%)";
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
+    ctx.moveTo(0.5 * width, 0.5 * height); // center
+    ctx.lineTo(xy.x, xy.y);
+    xy = getXY(i + 1, 0.3);
+    ctx.lineTo(xy.x, xy.y);
+    xy = getXY(i, 0.37);
+    ctx.fillText(skills[pentagonIndex].captions[valueIndex], xy.x, xy.y + 5);
+    valueIndex++;
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  valueIndex = 0;
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.3)";
+  ctx.lineWidth = 5;
+  var value = skills[pentagonIndex].values[valueIndex];
+  xy = getXY(i, value * 0.3);
+  ctx.moveTo(xy.x, xy.y);
+
+  /*** SKILL GRAPH ***/
+  for (var i = 0; i < skills[pentagonIndex].captions.length; i++) {
+    xy = getXY(i, value * 0.3);
+    ctx.lineTo(xy.x, xy.y);
+    valueIndex++;
+    value = skills[pentagonIndex].values[valueIndex];
+  }
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+  valueIndex = 0;
+  pentagonIndex++;
+});
