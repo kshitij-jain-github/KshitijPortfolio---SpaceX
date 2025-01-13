beea626b77a9f048f0b08ff5fa5c48d6eaec6e81
Kshitij Jain <jainkshitij1530@gmail.com>
2025-01-13 11:39:21 +0530

    ADDED-SWIPER-PROJECT&CERTIFICATE-TEMPLATES

--- site.js
+++ site.js
@@ -1,1 +1,28 @@
- 
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+
+
+
+//   CERTIFICATE SWIPER
+var swiper1 = new Swiper(".swiper1", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+    speed: 500,
+    loop: true,
+    rotate: true,
+    mousewheel: {
+    invert: false,
+  },
+});
